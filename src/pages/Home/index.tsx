c971ef073ff568de9ad926670b6fa0f96919bc13
SitaGomes <ansitagomes@gmail.com>
2022-02-08 21:00:22 +0100

    inicial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Styles from "home.module.scss"
+
+export const Home = (): JSX.Element => {
+  return (
+    <div className="App">
+      <h1>CellMark</h1>
+    </div>
+  );
+}
